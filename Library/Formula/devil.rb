d419b59ffc322a26298cc39e7d8fad0f9c5dd999
Andre Arko <andre@arko.net>
2009-10-19 12:15:35 -0700

    DevIL formula now includes ILU and ILUT
    Hopefully this will allow the gem to build

--- Library/Formula/devil.rb
+++ Library/Formula/devil.rb
@@ -4,9 +4,24 @@ class Devil <Formula
   url 'http://prdownloads.sourceforge.net/openil/1.7.8/DevIL-1.7.8.tar.gz'
   homepage 'http://sourceforge.net/projects/openil/'
   md5 '7918f215524589435e5ec2e8736d5e1d'
+  
+  def patches
+    DATA
+  end
 
   def install
-    system "./configure", "--prefix=#{prefix}", "--disable-debug", "--disable-dependency-tracking"
+    system "./configure", "--prefix=#{prefix}", "--enable-ILU", "--enable-ILUT"
     system "make install"
   end
 end
+__END__
+--- /src-ILU/ilur/ilur.c
++++ (clipboard)
+@@ -1,6 +1,6 @@
+ #include <string.h>
+ #include <stdio.h>
+-#include <malloc.h>
++// #include <malloc.h>
+ 
+ #include <IL/il.h>
+ #include <IL/ilu.h>
